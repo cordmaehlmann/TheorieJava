4b0039b01eaec972294712d4440502f59c0daaf7
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-09-30 09:09:59 +0200

    Create _010_ZweiZahlenGenauVergleichen.java

---
+++ src/Uebungen/_010_ZweiZahlenGenauVergleichen.java
@@ -0,0 +1,16 @@
+package Uebungen;
+
+public class _010_ZweiZahlenGenauVergleichen {
+
+    public static void main(String[] args) {
+
+        /*
+         * Zwei Zahlen genau vergleichen
+         *
+         * Schreibe ein Programm, das zwei zufällige Zahlen erzeugt.
+         * Dann soll das Programm testen und ausgeben,
+         * welche von den beiden Zahlen größer ist oder ob beide Zahlen gleich groß sind.
+         */
+
+    }
+}
