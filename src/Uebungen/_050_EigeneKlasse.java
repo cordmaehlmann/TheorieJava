558f9402d1d038889322921a7b6a6facc3e60ac7
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-10-01 15:11:27 +0200

    Create _050_EigeneKlasse.java

---
+++ src/Uebungen/_050_EigeneKlasse.java
@@ -0,0 +1,20 @@
+package Uebungen;
+
+public class _050_EigeneKlasse {
+
+    /*
+     * Eigene Klasse
+     *
+     * Überlege dir ein Objekt aus deiner Umgebung oder deiner Fantasie.
+     * Schreibe eine Klasse zum Erzeugen eines solchen Objekts.
+     * Anforderungen:
+     *     - Mindestens zwei Attribute
+     *     - Diverse Konstruktoren
+     *     - Kapselung mit private, Getter- & Setter-Methoden
+     *     - Statischer Counter
+     *     - toString()-Methode
+     *     - Mindestens eine zusätzliche Methode
+     *     - Die Klasse in eine getrennte Datei (Abgabe als Package/Ordner mit zwei Dateien)
+     */
+
+}
