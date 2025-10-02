76f92bedd8595dd15f604bdcec04a14e86c405cc
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-10-02 15:03:32 +0200

    Create _070_Quadratzahlen.java

---
+++ src/Uebungen/_070_Quadratzahlen.java
@@ -0,0 +1,15 @@
+package Uebungen;
+
+public class _070_Quadratzahlen {
+
+    /*
+     * Quadratzahlen
+     *
+     * Schreibe ein Programm, das alle Quadratzahlen von natürlichen
+     * Zahlen (1, 2, 3, ...) ausgibt, die kleiner als 100 sind.
+     * (Die Quadratzahlen sollen kleiner 100 sein!)
+     *
+     * Zusatz: Gib auch die Anzahl der gefundenen Quadratzahlen aus
+     */
+
+}
