512c582beb3a9a73244472285d4bbbd03e3747d0
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-10-02 15:11:35 +0200

    Create _080_Passwortgenerator.java

---
+++ src/Uebungen/_080_Passwortgenerator.java
@@ -0,0 +1,15 @@
+package Uebungen;
+
+public class _080_Passwortgenerator {
+
+    /*
+     * Passwortgenerator
+     *
+     * Schreibe eine Methode, die ein Passwort zurückgibt,
+     * das mindestens jeweils einmal Ziffer, Sonderzeichen, Klein- und Großbuchstabe enthält.
+     * Als Argument soll man einen Integer übergeben können,
+     * der die länge des Passworts bestimmt.
+     * Bei einem Argument kleiner acht soll das Passwort acht Zeichen haben.
+     */
+
+}
