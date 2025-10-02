b9624d14c79ba0fb902448f3331f2cb22b784d5e
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-10-02 15:56:14 +0200

    Create _090_Bubblesort.java

---
+++ src/Uebungen/_090_Bubblesort.java
@@ -0,0 +1,21 @@
+package Uebungen;
+
+public class _090_Bubblesort {
+
+    /*
+     * Bubblesort
+     *
+     * Schreibe eine Methode,
+     * der man ein Array mit beliebig vielen Integern als Werten übergeben kann
+     * und die dieses Array sortiert und zurückgibt.
+     *
+     * Benutze hierzu den Bubblesort-Algorithmus.
+     * Bei diesem wird das Array durchlaufen
+     * und jede Zahl mit der jeweils nachfolgenden Zahl verglichen.
+     * Wenn die nachfolgende Zahl kleiner ist,
+     * werden die Zahlen getauscht.
+     * Das Array wird so lange durchlaufen,
+     * bis bei einem Durchlauf keine Zahlen getauscht werden müssen.
+     */
+
+}
